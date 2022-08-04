ba94910ed00a831a0d8386236ce50d97a473921d
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-04 17:06:49 +0100

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include "mbed.h"
 
+struct IntVector {
+    int x;
+    int y;
+};
+
 int main() {
-    while (true) {
-        sleep();
-    }
+    IntVector p;
+    
+    p.x = -3;
+    p.y = 1;
+    
+    printf("(%d, %d)\r\n", p.x, p.y);
+    
+    // Do nothing, forever, when the program is complete
+    while (true) sleep();
 }
